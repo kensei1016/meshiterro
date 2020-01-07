b374a957b9fa9517db7cf6ed85bc03a5e4800f3e
kensei1016 <kensei1016ab@gmail.com>
2020-01-08 03:03:25 +0900

    Refileのseacret_keyを追加

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'feca5e1866cce33ba6d0a6b48773ab8c169eff3c352ff20e6148fcf44d8bfcf536a8898bb72cbcf56b64b85dca94a69aa50f48c26271c09c27ddfbccd004184b'
