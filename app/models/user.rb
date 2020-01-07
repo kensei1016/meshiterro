6a0aa80d5ac37bea4754b496be3889796c42221e
野田健成 <kensei1016ab@gmail.com>
2020-01-07 17:47:13 +0900

    プロフィール編集機能実装完了

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,7 @@ class User < ApplicationRecord
   has_many :post_images, dependent: :destroy
   has_many :post_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
+
+  # refileで画像のカラムを指定（カラム名から_idを除いたもの）
+  attachment :profile_image
 end
